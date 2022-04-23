737934693f56bee6cf214310186b7d3246041e0f
David Ye <dly8@case.edu>
2022-04-23 11:25:32 -0400

    added list of tasks

--- testing.js
+++ testing.js
@@ -2,21 +2,21 @@ var fs = require('fs');
 var c = require('./c-converter');
 
 const folder = 'tests/'
-let c_code = '#include <stdio.h>\nint main() {\n   // printf() displays the string inside quotation\n   printf("Hello, World!");\n   return 0;\n}';
+let c_code = 'a = b(c, d);';
 
 
-//console.log(c.convert(c_code));
+console.log(c.convert(c_code));
 
-fs.readdir(folder, (err, files) => {
-	if (err) console.error('nope', err);
-	files.filter(f => f.endsWith('.c')).slice(0, 7).forEach((fname) => {
-		fs.readFile(folder + fname, 'utf-8', (err, data) => {
-			if (err) console.error('nope', err);
-			console.log(`----------------------------------------------------------------------------------\nC:\n${data}\nPYTHON:\n`)
-			console.log(`${c.convert(data)}\n----------------------------------------------------------------------------------`);
-		})
-	});
-});
+// fs.readdir(folder, (err, files) => {
+// 	if (err) console.error('nope', err);
+// 	files.filter(f => f.endsWith('.c')).slice(0, 7).forEach((fname) => {
+// 		fs.readFile(folder + fname, 'utf-8', (err, data) => {
+// 			if (err) console.error('nope', err);
+// 			console.log(`----------------------------------------------------------------------------------\nC:\n${data}\nPYTHON:\n`)
+// 			console.log(`${c.convert(data)}\n----------------------------------------------------------------------------------`);
+// 		})
+// 	});
+// });
 
 //TODO test 8: multi line comments
 //TODO test 10: it's huge
